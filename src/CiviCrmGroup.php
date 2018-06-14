b791ddb72bc2ab872aaa7a781d0f656e894301b4
Christophe <christophe@colorfield.eu>
2018-06-14 14:19:23 +0200

    Get all groups for a contact

--- src/CiviCrmGroup.php
+++ src/CiviCrmGroup.php
@@ -3,6 +3,7 @@
 namespace Drupal\civicrm_tools;
 
 use Drupal\civicrm\Civicrm;
+use Drupal\Core\Database\Database;
 
 /**
  * Class CiviCrmGroup.
@@ -21,14 +22,51 @@ class CiviCrmGroup implements CiviCrmGroupInterface, CiviCrmEntityFormatInterfac
    *
    * @var \Drupal\civicrm_tools\CiviCrmApiInterface
    */
-  protected $civicrmToolsApi;
+  protected $civiCrmApi;
 
   /**
    * Constructs a new CiviCrmGroup object.
    */
-  public function __construct(Civicrm $civicrm, CiviCrmApiInterface $civicrm_tools_api) {
+  public function __construct(Civicrm $civicrm, CiviCrmApiInterface $civicrm_api) {
     $this->civicrm = $civicrm;
-    $this->civicrmToolsApi = $civicrm_tools_api;
+    $this->civiCrmApi = $civicrm_api;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getGroupsFromContact($contact_id, $load = TRUE) {
+    $result = [];
+    Database::setActiveConnection('civicrm');
+    $db = Database::getConnection();
+    $query = $db->query("SELECT group_id FROM {civicrm_group_contact} WHERE contact_id = :contact_id", [
+      ':contact_id' => $contact_id,
+    ]);
+    $queryResult = $query->fetchAll();
+    // Switch back to the default database.
+    Database::setActiveConnection();
+
+    if ($load) {
+      foreach ($queryResult as $row) {
+        $group = $this->civiCrmApi->get('Group', ['group_id' => $row->group_id]);
+        $result[$row->group_id] = $group;
+      }
+    }
+    else {
+      foreach ($queryResult as $row) {
+        $result[$row->group_id] = $row->group_id;
+      }
+    }
+    // https://issues.civicrm.org/jira/browse/CRM-20711
+    // https://issues.civicrm.org/jira/browse/CRM-18675
+    // $params = [
+    // 'contact_id' => $contact_id,
+    // ];
+    // $groups = $this->civiCrmApi->getAll('GroupContact', $params);
+    // if(!empty($groups)) {
+    // $result = $groups;
+    // }.
+    return $result;
   }
 
   /**
--- src/CiviCrmGroupInterface.php
+++ src/CiviCrmGroupInterface.php
@@ -7,5 +7,17 @@ namespace Drupal\civicrm_tools;
  */
 interface CiviCrmGroupInterface {
 
+  /**
+   * Get all groups for a contact.
+   *
+   * @param int $contact_id
+   *   CiviCRM contact id.
+   * @param bool $load
+   *   Load the CiviCRM Group.
+   *
+   * @return array
+   *   List of groups id's.
+   */
+  public function getGroupsFromContact($contact_id, $load);
 
 }
