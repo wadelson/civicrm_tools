b32f9b915806fb451ade41a8b502576e9f953a9f
Christophe <christophe@colorfield.eu>
2018-06-14 18:14:50 +0200

    Add exception handling

--- civicrm_tools_rest/src/Controller/UserRestController.php
+++ civicrm_tools_rest/src/Controller/UserRestController.php
@@ -49,31 +49,42 @@ class UserRestController extends ControllerBase {
   public function getUsersByGroup($group_id) {
     $result = [
       'data' => [],
-      'message' => '',
+      'message' => $this->t('No users for group id @group_id.', [
+        '@group_id' => $group_id,
+      ]),
     ];
-    $contacts = $this->civicrmToolsContact->getFromGroups([$group_id]);
+
+    $contacts = [];
+
+    // Try to get contacts from a Group.
+    try {
+      $contacts = $this->civicrmToolsContact->getFromGroups([$group_id]);
+      $result['message'] = '';
+    }catch (\Exception $exception) {
+      $result['message'] = $exception->getMessage();
+    }
+
+    // Try to get contacts from a Smart Group.
+    try {
+      $contacts = $this->civicrmToolsContact->getFromSmartGroup($group_id, []);
+      $result['message'] = '';
+    }catch (\Exception $exception) {
+      $result['message'] = $exception->getMessage();
+    }
+
     if (!empty($contacts)) {
       foreach ($contacts as $cid => $contact) {
         $user = $this->civicrmToolsContact->getUserFromContactId((int) $cid, CIVICRM_DOMAIN_ID);
         // A contact match could not exist for a user.
         if (!empty($user) && $user instanceof User) {
-          $result['data'][] = [
-            // @todo provide user entity
-            'id' => $user->id(),
-            'name' => $user->getUsername(),
-            'mail' => $user->getEmail(),
-          ];
+          $result['data'][] = $user->toArray();
         }
       }
     }
-    else {
-      // Set the default response to be returned if no results can be found.
-      $result['message'] = $this->t('No users for group id @group_id.', [
-        '@group_id' => $group_id,
-      ]);
-    }
+
     // Add the user_list cache tag to update when users are updated.
     $cacheMetadata = new CacheableMetadata();
+    // @todo needs to know modification to groups or set at least a max age.
     $cacheMetadata->setCacheTags(['user_list',]);
     // User url.path for group id.
     $cacheMetadata->setCacheContexts(['url.path']);
